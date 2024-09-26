360d04359b4484487170227c5b117aba1a39dbde
Leonardo <leozavglz@gmail.com>
2024-09-25 21:49:35 -0600

    ¡Demos demos demos demos!

---
+++ OpenCV/build/intermediates/cxx/Debug/5n312735/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Android_SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\leoza\\OneDrive\\Documentos\\Demos_SaVR\\Demo_SaVR_1\\OpenCV\\.cxx\\Debug\\5n312735\\x86_64" ^
+  opencv_jni_shared
---
+++ app/src/main/java/com/example/demo_savr_1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.demo_savr_1;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        if (OpenCVLoader.initLocal()) {
+            Log.i("OpenCV", "OpenCV successfully loaded.");
+        }
+    }
+}
